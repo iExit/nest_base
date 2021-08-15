e974caa213a0923706af8eddef58ed47cc234210
Shen Hao <775308609@qq.com>
2021-08-15 22:45:09 +0800

    🔧 配置Log4js日志

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,18 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { Log4jsLogger } from '@nestx-log4js/core';
 import { AppModule } from './app.module';
 
 const port = 3000;
+const logger = new Logger('main.ts');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  /**
+   * 配置Swagger
+   */
   const options = new DocumentBuilder()
     .setTitle('nest基础框架')
     .setDescription('nest基础框架Api文档')
@@ -15,6 +21,13 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
 
+  /**
+   * 使用log4js日志框架
+   */
+  app.useLogger(app.get(Log4jsLogger));
+
   await app.listen(port);
 }
-bootstrap();
+bootstrap().then(() => {
+  logger.log(`listen in http://localhost:${port}`);
+});
