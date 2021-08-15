8bd3097c86ba6a8ec3eb37d76d7627f036ac72f1
Shen Hao <775308609@qq.com>
2021-08-15 22:51:46 +0800

    🚚 移动user模块

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DbModule } from './db/db.module';
-import { UserModule } from './user/user.module';
+import { UserModule } from './modules/user/user.module';
 import { Log4jsModule } from '@nestx-log4js/core';
 
 @Module({
